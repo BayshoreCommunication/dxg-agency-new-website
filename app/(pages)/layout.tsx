6641f45fe4d430d97d5ce19e06434c7615d14777
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2025-01-08 12:13:06 +0600

    Update layout.tsx

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -37,11 +37,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full scroll-smooth bg-black ">
+    <html lang="en" className="h-full scroll-smooth bg-black font-sans">
       <body
         className={cn(
           "relative h-full overflow-x-hidden font-sans antialiased",
-          inter.className,
         )}
       >
         <Navbar />
