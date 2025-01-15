f46c5ffb415306200c7d6348d0145f708e98407a
rahad-dev24 <rahad.dev24@gmail.com>
2025-01-15 12:48:30 +0600

    test3

--- app/(pages)/post/[slug]/_getPost.js
+++ app/(pages)/post/[slug]/_getPost.js
@@ -1,6 +1,6 @@
 const postData = fetch("https://backend-dxgwebsite.vercel.app/site/blog", {
   cache: "force-cache",
-  //next: { revalidate: 3600 },
+  next: { revalidate: 3600 },
 }).then((res) => res.json());
 
 export default postData;
