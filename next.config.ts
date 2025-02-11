cf8ca8409bdd9c71eb6eeff5e97cd157a4352599
rahad-dev24 <rahad.dev24@gmail.com>
2025-02-11 10:06:23 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,38 +6,38 @@ const nextConfig: NextConfig = {
     // formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-        
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "drive.google.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "bayshore.nyc3.digitaloceanspaces.com",
+        // hostname: "**",
       },
     ],
   },
   skipTrailingSlashRedirect: true,
-  eslint: {
-    
-  },
-  
-  
+  eslint: {},
 };
 
 export default nextConfig;
