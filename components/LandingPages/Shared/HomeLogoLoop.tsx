7f65c1df7a005b8731baf4209614b6b4a77c981e
uiabu <ui.abukawsar@gmail.com>
2025-05-12 17:02:45 +0600

    Update Testimonial component: enhance client title and revert background color for improved clarity

--- components/LandingPages/Shared/HomeLogoLoop.tsx
+++ components/LandingPages/Shared/HomeLogoLoop.tsx
@@ -14,203 +14,203 @@ const HomeLogoLoop = () => {
                 alt="photoshop"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-2.png"
                 alt="google-adwords"
                 width={200}
                 height={200}
-                className="w-[140px]  h-auto "
+                className="w-[110px]  h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-3.png"
                 alt="Javascript"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto"
+                className="w-[110px] h-auto"
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-4.png"
                 alt="blender"
                 width={200}
                 height={200}
-                className="w-[140px]  h-auto"
+                className="w-[110px]  h-auto"
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-5.png"
                 alt="illustrator"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-6.png"
                 alt="Bing-ads"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-7.png"
                 alt="next-js"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-8.png"
                 alt="fb-ads"
                 width={200}
                 height={200}
-                className="w-[140px]  h-auto bg"
+                className="w-[110px]  h-auto bg"
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-9.png"
                 alt="maya"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />{" "}
               <Image
                 src="/landing-pages/my-event/clients/logo-10.png"
                 alt="semrush"
                 width={200}
                 height={200}
-                className="w-[140px]  h-auto bg"
+                className="w-[110px]  h-auto bg"
               />{" "}
               <Image
                 src="/landing-pages/my-event/clients/logo-11.png"
                 alt="AE"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-12.png"
                 alt="laravel"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-13.png"
                 alt="linkedin-ads"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-14.png"
                 alt="ppro"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-15.png"
                 alt="node-js"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-16.png"
                 alt="keyshot"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-17.png"
                 alt="google-trends"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-18.png"
                 alt="google-analytics"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-19.png"
                 alt="figma"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-20.png"
                 alt="google-search-console"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-21.png"
                 alt="google-tag-manager"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-22.png"
                 alt="ahrefs"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-23.png"
                 alt="ahrefs"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-24.png"
                 alt="ahrefs"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-25.png"
                 alt="ahrefs"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-26.png"
                 alt="ahrefs"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-27.png"
                 alt="ahrefs"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-28.png"
                 alt="ahrefs"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
               <Image
                 src="/landing-pages/my-event/clients/logo-29.png"
                 alt="ahrefs"
                 width={200}
                 height={200}
-                className="w-[140px] h-auto "
+                className="w-[110px] h-auto "
               />
             </div>
           </div>
--- components/LandingPages/Shared/Testimonial.tsx
+++ components/LandingPages/Shared/Testimonial.tsx
@@ -5,18 +5,23 @@ import React from "react";
 import HomeLogoLoop from "./HomeLogoLoop";
 
 const infos = {
-  title: "Our Clients ",
+  title: " We have been trusted to support some amazing brands",
   description: `The organizations below trusted DXG's expertise, precise execution, and proactive collaboration to elevate their event experience.`,
 };
 
 function Testimonial() {
   return (
-    <div className="bg-differentBackground py-8 lg:py-14">
+    <div className="bg-white py-8 lg:py-14">
       <MaxWidthWrapper>
         <div className="flex flex-col items-center justify-center">
           <div className=" max-w-3xl text-center ">
-            <Title className="text-center" text={infos.title} />
-            <Paragraph key={0} text={infos.description} />
+            <Title className="text-center text-black" text={infos.title} />
+            <h2></h2>
+            <Paragraph
+              className="text-black"
+              key={0}
+              text={infos.description}
+            />
           </div>
 
           <div className="relative w-full ">
