c5153f78b4c88e602e91eb1371a7099fcd03147d
uiabu <ui.abukawsar@gmail.com>
2025-05-18 12:24:30 +0600

    feat: update event gallery data and enhance description in PortfolioBreadCrumbSection

--- components/Sections/PortfolioSections/PortfolioBreadCrumbSection.tsx
+++ components/Sections/PortfolioSections/PortfolioBreadCrumbSection.tsx
@@ -10,7 +10,7 @@ export const PortfolioBreadCrumbSection = () => {
         variant: "caps",
       }}
       buttonTitle="Book An Appointment"
-      description="At the core of our ethos, we believe that the essence of our work transcends the boundaries of what we do and how we do it. It's about who we do it with, for, and why we do it. This belief is profoundly woven into every aspect of our business, from our attentive process and unparalleled service to our purpose-driven mission and the vast experience we offer."
+      description="At DXG Agency, we believe that every event tells a unique story. Our portfolio is a vibrant mix of corporate gatherings, private parties, brand activations, and so much more. Each crafted with creativity, attention to detail, and a whole lot of heart. From the initial idea to the final execution, we bring your visions to life, creating unforgettable experiences. Dive into our work and discover how we turn fleeting moments into lasting memories."
     />
   );
 };
